3aae72d828915a8b40ca4af1c4bcdc9000bd0838
thanhvp <phuocthanh20cgh@gmail.com>
2023-07-07 01:24:54 +0700

    update

--- routes/playerRouter.js
+++ routes/playerRouter.js
@@ -2,15 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const playerController = require("../controllers/playerController");
 const multer = require("multer");
-const {ensureAuthenticated, jwtAuth} = require('../config/auth');
-const {requireRole} = require('../config/verifyRole');
+const { ensureAuthenticated, jwtAuth } = require('../config/auth');
+const { requireRole } = require('../config/verifyRole');
 const cloudinary = require('cloudinary').v2;
 
 // Cấu hình kết nối với Cloudinary
-cloudinary.config({ 
-  cloud_name: 'drvrfmcji', 
-  api_key: '955699232226773', 
-  api_secret: 'Fl8t8OdH_3Lo5Ke8hj4kz1tg34g' 
+cloudinary.config({
+  cloud_name: 'drvrfmcji',
+  api_key: '955699232226773',
+  api_secret: 'Fl8t8OdH_3Lo5Ke8hj4kz1tg34g'
 });
 
 const upload = multer({
@@ -19,14 +19,34 @@ const upload = multer({
 
 const playerRouter = express.Router();
 playerRouter.use(bodyParser.json());
+
+// Middleware để upload ảnh lên Cloudinary
+const uploadImage = async (req, res, next) => {
+  const file = req.file;
+  if (file) {
+    try {
+      const result = await cloudinary.uploader.upload(file.path);
+      req.imageUrl = result.secure_url;
+    } catch (error) {
+      console.error(error);
+      req.flash("error_msg", "Upload failed");
+      return res.redirect("/players");
+    }
+  }
+  next();
+};
+
 playerRouter
   .route("/")
   .get(jwtAuth, requireRole, playerController.index)
-  .post(jwtAuth, requireRole, upload.single("file"), playerController.create);
+  .post(jwtAuth, requireRole, upload.single("file"), uploadImage, playerController.create);
+
 playerRouter
   .route("/edit/:playerId")
   .get(jwtAuth, requireRole, playerController.formEdit)
-  .post(jwtAuth, requireRole, upload.single("file"), playerController.edit);
+  .post(jwtAuth, requireRole, upload.single("file"), uploadImage, playerController.edit);
+
 playerRouter.route("/:playerId").get(playerController.playerDetail);
 playerRouter.route("/delete/:playerId").get(jwtAuth, requireRole, playerController.delete);
+
 module.exports = playerRouter;
